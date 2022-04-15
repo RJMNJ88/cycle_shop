34a968d01818d2e53f57aa6327e1636737797cc9
RJMNJ88 <90220183+RJMNJ88@users.noreply.github.com>
2022-04-15 13:49:27 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,57 @@
+let menuBtn = document.querySelector('#menu-btn');
+let menuBox = document.querySelector('.sidebar');
+let loginBtn = document.querySelector('#login-btn');
+let loginForm = document.querySelector('.login');
+let missionSection = document.querySelector('.mission');
+let servicesSection = document.querySelector('.services');
+let equipmentSection = document.querySelector('.equipment');
+let roadSection = document.querySelector('.road');
+
+
+menuBtn.onclick = () => {
+    menuBox.classList.toggle('collapse');
+    missionSection.classList.toggle('active');
+    servicesSection.classList.toggle('active');
+    equipmentSection.classList.toggle('active');
+    roadSection.classList.toggle('active');
+}
+
+loginBtn.onclick = () => {
+    loginForm.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    loginForm.classList.remove('active');
+}
+
+// Dialog box
+let modalOne = document.getElementById('modal-one');
+let modalTwo = document.getElementById('modal-two');
+let modalThree = document.getElementById('modal-three');
+let modalFour = document.getElementById('modal-four');
+let modalFive = document.getElementById('modal-five');
+let modalSix = document.getElementById('modal-six');
+let closeModal = document.querySelector('#modal-close')
+let modal = document.querySelector('#modal');
+
+modalOne.onclick = () => {
+    modal.showModal();
+}
+modalTwo.onclick = () => {
+    modal.showModal();
+}
+modalThree.onclick = () => {
+    modal.showModal();
+}
+modalFour.onclick = () => {
+    modal.showModal();
+}
+modalFive.onclick = () => {
+    modal.showModal();
+}
+modalSix.onclick = () => {
+    modal.showModal();
+}
+closeModal.onclick = () => {
+    modal.close();
+}
